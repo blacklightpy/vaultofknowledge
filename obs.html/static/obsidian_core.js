6c6b9a05920c98a651d70ac82fd4e800d49549f9
blacklightpy <blacklightpy@users.noreply.github.com>
2025-09-24 15:00:06 +0000

    Build: dbfae7b35efb48a921398af8f9d40dee76c8a89d (with sitemap) dbfae7b35efb48a921398af8f9d40dee76c8a89d

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '324901358061772640310796860079233039339'                       // used to check whether the localStorage data is stale
+var gzip_hash = '87715217258355470682380759592713832827'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
